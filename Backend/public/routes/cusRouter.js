e24e1d89e5942b6054acdcee3cbf89c2952adcc6
정현성 <165395044+dau-J@users.noreply.github.com>
2025-03-13 04:51:06 +0900

    Delete Backend directory

--- Backend/public/routes/cusRouter.js
+++
@@ -1,9 +0,0 @@
-// cusRouter.js
-const express = require('express');
-const router = express.Router();
-const cusController = require('../api/cusController');
-
-// 고객 회원가입
-router.post('/cus', cusController.register);
-
-module.exports = router;
